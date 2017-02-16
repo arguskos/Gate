52de749de1a409918968080756558456375e1abc
Weraine <arne.wesenbeek@student.kdg.be>
2017-02-16 12:37:36 +0100

    package machine

---
+++ PROJECT/Assets/Supervisor and machinist contr/scripts/packager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class packager : MonoBehaviour {
+
+    private bool objectInPacker = false;
+    private Collider toPackObj;
+    public GameObject  thisMachine;
+    public GameObject packedPackage;
+    public float machineSpeed = 0.005F;
+    public float maxHeight = 1.605F;
+    public float minHeight = 0.982F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Machinable")
+        {
+            toPackObj = other;
+            objectInPacker = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Machinable")
+        {
+            objectInPacker = false;
+        }
+    }
+    void buttonPress(string input)
+    {
+        //minwaard 0.982
+        //maxwaarde 1.605
+        if (input == "up" && transform.position.y <= maxHeight)
+        {
+            Debug.Log("upwego");
+            transform.Translate(Vector3.forward * machineSpeed);
+        }
+        if (input == "down" && transform.position.y >= minHeight)
+        {
+            Debug.Log("upwego");
+            transform.Translate(Vector3.back * machineSpeed);
+        }
+        if (objectInPacker == true)
+        {
+            Debug.Log("woop i git it");
+
+            if (input == "act1")
+            {
+                //pack package
+                packPac();
+
+            }
+            else if (input == "act2")
+            {
+                //miss verpakking vernielen ? 
+            }
+        }
+
+    }
+    void packPac() 
+    {
+        Vector3 pos = toPackObj.transform.position;
+        Quaternion rot = toPackObj.transform.rotation;
+        Destroy(toPackObj.gameObject);
+        Instantiate(packedPackage,pos,rot);
+    }
+}
