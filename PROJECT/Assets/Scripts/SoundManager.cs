91a4e195b63b09cf4c66c4594453c5c7cb97d12d
WouterWeynants <wouter.weynants@hotmail.com>
2017-02-17 10:03:38 +0100

    Work in progress Sound Manager

---
+++ PROJECT/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource[] sounds = new AudioSource[10];
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PlaySound(string soundName)
+    {
+        for(int i = 0; i < sounds.Length; i++)
+        {
+            //Do Nothing
+        }
+    }
+}
